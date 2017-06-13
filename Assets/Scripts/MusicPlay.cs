338f0cd2af4c09d630c4da858601778f4108d454
furikake <ewave6300@gmail.com>
2017-06-14 02:16:24 +0900

    MusicPlayの整形
    ロードと再生が同じになっていたのを分割。
    与えられたMusicDataから音楽ファイルやノーツ情報をロードする関数LoadMusicを追加。
    コード整形。

--- Assets/Scripts/MusicPlay.cs
+++ Assets/Scripts/MusicPlay.cs
@@ -11,9 +11,6 @@ public class MusicPlay : MonoBehaviour {
 
     //グローバル変数群
     MusicData nowMusic;         //現在再生しているMusicのData
-    AudioSource audioSource;    //このgameObjectに紐付けられたAudioSource(Required)
-    Key[] keys; //鍵（けん）
-
     public MusicData NowMusic
     {
         get
@@ -24,21 +21,61 @@ public class MusicPlay : MonoBehaviour {
         set
         {
             nowMusic = value;
+            LoadMusic();
         }
     }
 
-    // Use this for initialization
+    AudioSource audioSource;    //このgameObjectに紐付けられたAudioSource(Required)
+    Key[] keys; //鍵（けん）
+    
+    // Start関数
     private void Start () {
         //audioSource初期化
         audioSource = gameObject.GetComponent<AudioSource>();
 
         
     }
-
+    
+    // Update関数
     private void Update()
     {
-        //表示更新
-        RefreshDisplay();
+
+    }
+
+    /// <summary>
+    /// 音楽ファイル、およびノーツ配置をロード
+    /// </summary>
+    private void LoadMusic()
+    {
+        //もしnowMusicが存在しなかったら終了措置
+        if (nowMusic == null) return;
+        
+        //音楽のロード
+        StartCoroutine("StreamAudioFile", nowMusic.WavMain);
+
+        //鍵の情報取得
+
+        //ノーツ情報取得、各鍵に配置
+
+    }
+    /// <summary>
+    /// 音楽ファイルを非同期的に読み込み
+    /// </summary>
+    /// <param name="fileName">ファイル名</param>
+    /// <returns></returns>
+    IEnumerator StreamAudioFile(string fileName)
+    {
+        //ソース指定し音楽流す
+        //音楽ファイルロード
+        using (WWW www = new WWW("file:///" + fileName))
+        {
+            //読み込み完了まで待機
+            yield return www;
+
+            audioSource.clip = www.GetAudioClip(true, true);
+
+            audioSource.Play();
+        }
     }
 
     /// <summary>
@@ -52,37 +89,15 @@ public class MusicPlay : MonoBehaviour {
             return;
         }
         
-        //WavMainを再生（非同期処理）
-        StartCoroutine("StreamPlayAudioFile", nowMusic.WavMain);
+        //WavMainをロード（非同期処理）
+        StartCoroutine("StreamAudioFile", nowMusic.WavMain);
     }
 
     /// <summary>
-    /// 音楽ファイルを非同期的に読み込み
+    /// 秒を表すfloat型から「分:秒」の形のStringを出力
     /// </summary>
-    /// <param name="fileName">ファイル名</param>
-    /// <returns></returns>
-    IEnumerator StreamPlayAudioFile(string fileName)
-    {
-        //ソース指定し音楽流す
-        //音楽ファイルロード
-        using(WWW www = new WWW("file:///" + fileName))
-        {
-            //読み込み完了まで待機
-            yield return www;
-
-            audioSource.clip = www.GetAudioClip(true, true);
-
-            audioSource.Play();
-        }
-    }
-    
-    //表示更新
-    void RefreshDisplay()
-    {
-        
-    }
-
-    //float型を分秒表示に
+    /// <param name="time">時間（秒単位）</param>
+    /// <returns>「分:秒」の形の文字列（秒は2桁表記）</returns>
     string FloatToMinSecString(float time)
     {
         return ((int)(time / 60)).ToString("D2") + ":" + ((int)(time % 60)).ToString("D2");
