246113358790c9301a7bb51e46e677099107d40e
furikake <ewave6300@gmail.com>
2017-06-15 01:36:45 +0900

    MusicData各種修正
    ・MusicDataの変数群を、BMSの公式フォーマットに準拠
    ・インスタンス変数をカプセル化と明確に区別
    ・コンストラクタ（ヘッダ読み込み）を正規表現を使う方針からハイブリッド方針に変更
    ・コンストラクタのwav,bmp,bpmExの読み込み箇所は未完成

--- Assets/Scripts/MusicData.cs
+++ Assets/Scripts/MusicData.cs
@@ -7,30 +7,147 @@ using System.Text.RegularExpressions;
 /// </summary>
 public class MusicData {
 
-    //Values
-    public string WavMain { get; private set; } //メインの音楽ファイル(絶対パス)
-    public Dictionary<string, string> Wav { get; private set; }  //キータップ音などの音楽ファイル(ディクショナリ、絶対パス)
-    public int Bpm { get; private set; } //BPM
-    public string Title { get; private set; }    //曲名
-    public string Genre { get; private set; }    //ジャンル（メニューでの区分けに使われる）
-    public string Artist { get; private set; }   //作曲者名
-    public string DataFilePath { get; private set; } //楽譜データファイルへの絶対パス
+    //変数群
+    string _dataFilePath;   //楽譜データファイルへの絶対パス
+    public string DataFilePath { get{ return _dataFilePath; } }
+    
+    //ファイルから読み込むデータ群
+    //単一データ
+    int _player;        //プレイモード
+    string _genre;      //ジャンル
+    string _title;      //曲名
+    string _artist;     //制作者名
+    float _bpm;         //初期BPM
+    string _midifile;   //バックグラウンドで流す音声ファイルへの絶対パス
+    int _playlevel;     //難易度
+    int _rank;          //ランク
+    int _volWav;        //音量補正(%)
+    int _total;         //ゲージの増量
+    string _stageImage; //曲開始時の画像
 
-    //Constructor
+    public int Player { get{ return _player; } }
+    public string Genre { get{ return _genre; } }
+    public string Title { get{ return _title; } }
+    public string Artist { get{ return _artist; } }
+    public float Bpm { get{ return _bpm; } }
+    public string Midifile { get{ return _midifile; } }
+    public int Playlevel { get{ return _playlevel; } }
+    public int Rank { get{ return _rank; } }
+    public int VolWav { get{ return _volWav; } }
+    public int Total { get{ return _total; } }
+    public string StageImage { get { return _stageImage; } }
+
+    //配列データ（0-Zの36進数2桁で定義されている。Keyでは大文字を使うものとする）
+    Dictionary<string, string> _wav;    //音声ファイルへの絶対パス
+    Dictionary<string, string> _bmp;    //表示画像
+    Dictionary<string, float> _bpmEx;  //拡張BPM
+
+    public Dictionary<string, string> Wav { get{ return _wav; } }
+    public Dictionary<string, string> Bmp { get{ return _bmp; } }
+    public Dictionary<string, float> BpmEx { get{ return _bpmEx; } }
+    
+    //メソッド
 
     /// <summary>
     /// 空のコンストラクタです。（デフォルトBPMは130です）
     /// </summary>
     public MusicData() {
-        DataFilePath = "";
-        WavMain = "";
-        Wav = new Dictionary<string, string>();
-        Bpm = 130;
-        Title = "";
-        Genre = "";
-        Artist = "";
+        _dataFilePath = "";
+
+        _genre = "";
+        _title = "";
+        _artist = "";
+        _bpm = 130;
+        _midifile = "";
+        _playlevel = 0;
+        _rank = 0;
+        _volWav = 100;
+        _total = 0;
+
+        _wav = new Dictionary<string, string>();
+        _bmp = new Dictionary<string, string>();
+        _bpmEx = new Dictionary<string, float>();
     }
 
+    /// <summary>
+    /// 指定されたデータファイルからヘッダ データを読み込みます。
+    /// </summary>
+    /// <param name="dataFilePath">データファイルの絶対パス</param>
+    public MusicData(string dataFilePath)
+        : this()
+    {
+        //↑とりあえず最初はデフォルトコンストラクタで初期化
+        
+        //データファイルパス保存
+        _dataFilePath = dataFilePath;
+
+        //曲名はデフォルトだとデータファイルと同名になる
+        _title = Path.GetFileNameWithoutExtension(dataFilePath);
+
+        //エンコードは自動判別
+        using (StreamReader dataFileStream = new StreamReader(dataFilePath, true))
+        {
+            while (dataFileStream.Peek() >= 0)
+            {
+                //一行ずつ読み込む
+                string dataLine = dataFileStream.ReadLine();
+
+                //一文字目が「#」かどうかでコマンドコードを判別
+                if(dataLine[0] == '#')
+                {
+                    //「 (半角スペース)」、「:(コロン)」によって文字列をコマンドと内容物に分割
+                    char[] separators = { ' ', ':' };
+                    int sepIndex = dataLine.IndexOfAny(separators);
+                    string command = dataLine.Substring(1, sepIndex - 1).Trim();
+                    string content = dataLine.Substring(sepIndex + 1).Trim();
+
+                    //splitedData[0]にコマンド名が記載されているはずなので読取
+                    switch (command.ToUpper())
+                    {
+                        case "PLAYER":
+                            _player = int.Parse(content);
+                            break;
+                        case "TITLE":
+                            _title = content;
+                            break;
+                        case "GENRE":
+                            _genre = content;
+                            break;
+                        case "ARTIST":
+                            _artist = content;
+                            break;
+                        case "BPM":
+                            _bpm = float.Parse(content);
+                            break;
+                        case "MIDIFILE":
+                            _midifile = Path.GetDirectoryName(_dataFilePath) + content;
+                            break;
+                        case "PLAYLEVEL":
+                            _playlevel = int.Parse(content);
+                            break;
+                        case "RANK":
+                            _rank = int.Parse(content);
+                            break;
+                        case "VOLWAV":
+                            _volWav = int.Parse(content);
+                            break;
+                        case "TOTAL":
+                            _total = int.Parse(content);
+                            break;
+                        case "STAGEFILE":
+                            _stageImage = Path.GetDirectoryName(_dataFilePath) + content;
+                            break;
+                        default:
+                            //switch文で読み取れないコマンドの判別
+
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    /*
     /// <summary>
     /// 指定されたデータファイルからヘッダ データを読み込みます。
     /// </summary>
@@ -106,5 +223,5 @@ public class MusicData {
                 }
             }
         }
-    }
+    }*/
 }
