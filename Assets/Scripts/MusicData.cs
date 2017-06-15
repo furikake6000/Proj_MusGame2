2c0068e3149b0fb8a00b4c01fb4869180a5ac6a0
furikake <ewave6300@gmail.com>
2017-06-15 11:48:12 +0900

    MusicData修正
    若干のコメントの修正
    WAVxx、BMPxx、BPMxxの読み込みを実装

--- Assets/Scripts/MusicData.cs
+++ Assets/Scripts/MusicData.cs
@@ -9,6 +9,7 @@ public class MusicData {
 
     //変数群
     string _dataFilePath;   //楽譜データファイルへの絶対パス
+
     public string DataFilePath { get{ return _dataFilePath; } }
     
     //ファイルから読み込むデータ群
@@ -37,7 +38,7 @@ public class MusicData {
     public int Total { get{ return _total; } }
     public string StageImage { get { return _stageImage; } }
 
-    //配列データ（0-Zの36進数2桁で定義されている。Keyでは大文字を使うものとする）
+    //配列データ（0-Zの36進数2桁のTKeyを持つ。TKeyでは大文字を使うものとする）
     Dictionary<string, string> _wav;    //音声ファイルへの絶対パス
     Dictionary<string, string> _bmp;    //表示画像
     Dictionary<string, float> _bpmEx;  //拡張BPM
@@ -87,6 +88,7 @@ public class MusicData {
         //エンコードは自動判別
         using (StreamReader dataFileStream = new StreamReader(dataFilePath, true))
         {
+
             while (dataFileStream.Peek() >= 0)
             {
                 //一行ずつ読み込む
@@ -98,11 +100,11 @@ public class MusicData {
                     //「 (半角スペース)」、「:(コロン)」によって文字列をコマンドと内容物に分割
                     char[] separators = { ' ', ':' };
                     int sepIndex = dataLine.IndexOfAny(separators);
-                    string command = dataLine.Substring(1, sepIndex - 1).Trim();
-                    string content = dataLine.Substring(sepIndex + 1).Trim();
+                    string command = dataLine.Substring(1, sepIndex - 1).Trim().ToUpper();  //コマンド部。検索を用意にするため大文字に統一
+                    string content = dataLine.Substring(sepIndex + 1).Trim();   //内容部。
 
                     //splitedData[0]にコマンド名が記載されているはずなので読取
-                    switch (command.ToUpper())
+                    switch (command)
                     {
                         case "PLAYER":
                             _player = int.Parse(content);
@@ -140,88 +142,35 @@ public class MusicData {
                         default:
                             //switch文で読み取れないコマンドの判別
 
+                            //判定
+                            if (Regex.IsMatch(command, @"^\d{5}$"))
+                            {
+                                //メインデータ部（音符の定義）
+                                //現状ではヘッダ読み込み時にメインデータに関して行うアクションは特になし
+                            }
+                            else if (Regex.IsMatch(command, @"^WAV[0-9A-Z]{2}$"))
+                            {
+                                //WAVxx（音声ファイルの定義）
+                                _wav.Add(command.Substring(3, 2), Path.GetDirectoryName(_dataFilePath) + content);
+                            }
+                            else if (Regex.IsMatch(command, @"^BMP[0-9A-Z]{2}$"))
+                            {
+                                //BMPxx（画像ファイルの定義）
+                                _bmp.Add(command.Substring(3, 2), Path.GetDirectoryName(_dataFilePath) + content);
+                            }
+                            else if (Regex.IsMatch(command, @"^BPM[0-9A-Z]{2}$"))
+                            {
+                                //BPMxx（拡張bpmの定義）
+                                _bpmEx.Add(command.Substring(3, 2), float.Parse(content));
+                            }
+                            else
+                            {
+                                //どこにもひっかからない謎のコマンド
+                            }
                             break;
                     }
                 }
             }
         }
     }
-
-    /*
-    /// <summary>
-    /// 指定されたデータファイルからヘッダ データを読み込みます。
-    /// </summary>
-    /// <param name="dataFilePath">データファイルの絶対パス</param>
-    public MusicData(string dataFilePath)
-        : this()
-    {
-        //↑とりあえず最初はデフォルトコンストラクタで初期化
-
-        //曲名はデフォルトだとデータファイルと同名になる
-        Title = Path.GetFileNameWithoutExtension(dataFilePath);
-
-        //データファイルパス格納
-        DataFilePath = dataFilePath;
-
-        //検索用正規表現（ヘッダー部）
-        Regex regTitle = new Regex(@"^#TITLE\s.+$"); //TITLE 一文字以上の文字列
-        Regex regGenre = new Regex(@"^#GENRE\s.+$"); //GENRE 一文字以上の文字列
-        Regex regArtist = new Regex(@"^#ARTIST\s.+$");   //ARTIST 一文字以上の文字列
-        Regex regBpm = new Regex(@"^#BPM\s\d+$");    //BPM 一文字以上の数列（任意の自然数）
-        Regex regWavMain = new Regex(@"^#WAVMAIN\s.+\.(wav|ogg|mp3)$");    //WAV メインオーディオファイル読み込み（wav/ogg/mp3ファイル）
-        Regex regWav = new Regex(@"^#WAV[0-9A-Fa-f][0-9A-Za-z]\s.+\.(wav|ogg|mp3)$");    //WAV オーディオファイル読み込み（wav/ogg/mp3ファイル）
-
-        ////検索用正規表現（データ部）
-        //Regex regObjDef = new Regex(@"^#\d{3}[1-6][1-9]:([0-9A-Fa-f][0-9A-Za-z])+$");  //各種オブジェクト定義（定義部は36進数）
-        //Regex regBackDef = new Regex(@"^#\d{3}01:([0-9A-Fa-f][0-9A-Za-z])+$");  //バックコーラス定義（定義部は36進数）
-        //Regex regShortenTremolo = new Regex(@"^#\d{3}02:\d+(\.\d+)?$");   //小節の短縮（定義部は小数含む正数）
-        //Regex regChangeBpm = new Regex(@"^#\d{3}03:([0-9A-Fa-f]{2})+$");   //BPMの変更（定義部は16進数）
-
-        //エンコードは自動判別
-        using (StreamReader dataFileStream = new StreamReader(dataFilePath, true))
-        {
-            while (dataFileStream.Peek() >= 0)
-            {
-                //一行ずつ読み込む
-                string dataLine = dataFileStream.ReadLine();
-
-                //条件分岐部分
-                if (regTitle.IsMatch(dataLine))
-                {
-                    //TITLE
-                    Title = dataLine.Substring(7);
-                }
-                else
-                if (regGenre.IsMatch(dataLine))
-                {
-                    //GENRE
-                    Genre = dataLine.Substring(7);
-                }
-                else
-                if (regArtist.IsMatch(dataLine))
-                {
-                    //ARTIST
-                    Artist = dataLine.Substring(8);
-                }
-                else
-                if (regBpm.IsMatch(dataLine))
-                {
-                    //BPM
-                    Bpm = int.Parse(dataLine.Substring(6));
-                }
-                else
-                if (regWavMain.IsMatch(dataLine))
-                {
-                    //WAVMAIN
-                    WavMain = Path.GetDirectoryName(DataFilePath) + dataLine.Substring(9);
-                }
-                else
-                if (regWav.IsMatch(dataLine))
-                {
-                    //WAVXX(XXには36進数が入る、小文字は大文字に)
-                    Wav[dataLine.Substring(4, 2).ToUpper()] = Path.GetDirectoryName(DataFilePath) + dataLine.Substring(7);
-                }
-            }
-        }
-    }*/
 }
