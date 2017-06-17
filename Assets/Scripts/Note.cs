072a13407954cb43277009f89dcbf37ebfd9c828
furikake <ewave6300@gmail.com>
2017-06-17 10:43:05 +0900

    Note実装

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -3,5 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Note {
-    
+    float _time;
+    string _wavID;
+
+    public float Time{
+        get{ return _time; }
+
+        set{ _time = value; }
+    }
+
+    public string WavID{
+        get{ return _wavID; }
+
+        set{ _wavID = value; }
+    }
 }
